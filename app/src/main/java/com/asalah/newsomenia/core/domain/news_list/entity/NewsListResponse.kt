7c4a5c14810f76d501d4073001d471a0316ddd0d
Ahmed <ahmedsalah122331@gmail.com>
2023-03-12 08:41:54 +0200

    remove unused properties

--- app/src/main/java/com/asalah/newsomenia/core/domain/news_list/entity/NewsListResponse.kt
+++ app/src/main/java/com/asalah/newsomenia/core/domain/news_list/entity/NewsListResponse.kt
@@ -4,10 +4,6 @@ import com.squareup.moshi.Json
 import com.asalah.newsomenia.core.domain.news_items.entity.Article
 
 data class NewsListResponse(
-    @Json(name = "status")
-    val status: String? = null,
-    @Json(name = "totalResults")
-    val totalResults: Int? = null,
     @Json(name = "articles")
     val articles: List<Article>
 )
