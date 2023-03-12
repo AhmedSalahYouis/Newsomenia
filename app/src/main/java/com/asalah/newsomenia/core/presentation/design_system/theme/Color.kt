158eaf20119264bd9706031a1c7dd57d346331c5
Ahmed <ahmedsalah122331@gmail.com>
2023-03-12 07:22:45 +0200

    add styles

---
+++ app/src/main/java/com/asalah/newsomenia/core/presentation/design_system/theme/Color.kt
@@ -0,0 +1,23 @@
+package com.asalah.newsomenia.core.presentation.design_system.theme
+
+import androidx.compose.ui.graphics.Color
+
+val White = Color(0xFFFFFFFF)
+
+val Blue00 = Color(0xFF00BCD4)
+val Blue10 = Color(0xFF03A9F4)
+val Blue20 = Color(0xFF2196F3)
+val Blue30 = Color(0xFF3F51B5)
+val Blue40 = Color(0xFF2E3B83)
+
+val Green10 = Color(0xFF4CAF50)
+
+val Lemon = Color(0xFFCDDC39)
+
+val Gray00 = Color(0xFFFBFBFB)
+val Gray10 = Color(0xFFE5E5E5)
+val Gray20 = Color(0xFFC0C0C0)
+val Gray30 = Color(0xFF626262)
+val Gray40 = Color(0xFF222222)
+val Gray50 = Color(0xFF111111)
+
